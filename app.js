9cfb7451c1932fc3370998abc2a74eddcef5474d
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-21 16:02:39 -0400

    adds all of the buttons from the html

---
+++ app.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll(".number");
+
+const negativeButton = document.getElementById("negative");
+const backspaceButton = document.getElementById("backspace");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+const minusButton = document.getElementById("minus");
+const plusButton = document.getElementById("plus");
+const equalButton = document.getElementById("equal");
+
+let total = 0;
