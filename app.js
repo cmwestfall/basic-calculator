4b45c9932a4db955567c077d2718368bfc818472
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-21 16:21:17 -0400

    taking a break

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ const plusButton = document.getElementById("plus");
 const equalButton = document.getElementById("equal");
 
 let total = 0;
+let input = 0;
+
+numberButtons.forEach((button) => {
+  return parseInt(button.id);
+});
+
+const changeNegative = (number) => {
+  return "-" + number;
+};
+
+negativeButton.addEventListener("click", changeNegative);
