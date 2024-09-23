9eb0fe14f8f6b183657ecef35cc2c53271106632
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-23 03:08:19 -0400

    activates the number buttons so they work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const numberButtons = document.querySelectorAll(".number");
 
+const display = document.getElementById("display");
 const negativeButton = document.getElementById("negative");
 const backspaceButton = document.getElementById("backspace");
 const multiplyButton = document.getElementById("multiply");
@@ -10,6 +11,14 @@ const equalButton = document.getElementById("equal");
 
 let total = 0;
 let input = 0;
+
+// function for allowing the number buttons to work
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.value += button.id;
+    console.log(display);
+  });
+});
 
 const changeNegative = (number) => {
   return "-" + number;
