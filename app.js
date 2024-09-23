bc1082515104c18123155871e9d8a98dc933a120
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-23 06:19:02 -0400

    i think it finally works!

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operation");
 
 const display = document.getElementById("display");
 const negativeButton = document.getElementById("negative");
 const backspaceButton = document.getElementById("backspace");
-const multiplyButton = document.getElementById("multiply");
-const divideButton = document.getElementById("divide");
-const minusButton = document.getElementById("minus");
-const plusButton = document.getElementById("plus");
 const equalButton = document.getElementById("equal");
 const clearButton = document.getElementById("clear");
+const decimalButton = document.getElementById("decimal");
 
 let total = 0;
-let input = 0;
+let currentOperation = "";
 
-// function for allowing the number buttons to work
+// allows the number buttons to work
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    display.value += button.id;
+    if (display.value === "0") {
+      display.value = parseFloat(button.id);
+    } else {
+      display.value += parseFloat(button.id);
+    }
   });
 });
 
-const changeNegative = (number) => {
-  return "-" + number;
-};
+// allows there to be a 'current' operation to work with
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    total = parseFloat(display.value);
+    currentOperation = button.id;
+    display.value = 0;
+  });
+});
+
+//negativeButton
+negativeButton.addEventListener("click", () => {
+  if (display.value.includes("-")) {
+    display.value = display.value.toString().slice(1);
+  } else {
+    display.value = "-" + display.value;
+  }
+});
+
+// allows the user to input decimal numbers
+decimalButton.addEventListener("click", () => {
+  display.value += ".";
+});
 
 // backspace button for deleting mistakes
 backspaceButton.addEventListener("click", () => {
@@ -31,25 +52,21 @@ backspaceButton.addEventListener("click", () => {
 
 // clear button for clearing the display
 clearButton.addEventListener("click", () => {
-  display.value = "";
+  display.value = "0";
 });
 
-const multiply = (num1, num2) => {
-  total = num1 * num2;
-};
+// equal button functionality
+equalButton.addEventListener("click", () => {
+  if (currentOperation === "plus") {
+    display.value = total + parseFloat(display.value);
+    total = parseFloat(display.value);
+  } else if (currentOperation === "minus") {
+    display.value = total - parseFloat(display.value);
+  } else if (currentOperation === "multiply") {
+    display.value = total * parseFloat(display.value);
+  } else if (currentOperation === "divide") {
+    display.value = total / parseFloat(display.value);
+  }
 
-const divide = (num1, num2) => {
-  total = num1 / num2;
-};
-
-const subtract = (num1, num2) => {
-  total = num1 - num2;
-};
-
-const add = (num1, num2) => {
-  total = num1 + num2;
-};
-
-const equal = () => {};
-
-negativeButton.addEventListener("click", changeNegative);
+  currentOperation = "";
+});
